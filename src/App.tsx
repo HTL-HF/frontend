a94b87a0fd9a30b179f8b983918416a526424c82
yotam <yotam201410@gmail.com>
2024-08-11 19:33:14 +0300

    finished home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./views/HomePage";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
