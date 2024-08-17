af7d365d7036fb9a2b4d95d583a264d018ae7966
yotam201410 <yotam201410@gmail.com>
2024-08-17 23:40:48 +0300

    user redux auto set when login/register and when reloading

--- src/views/LoginPage.tsx
+++ src/views/LoginPage.tsx
@@ -1,80 +1,88 @@
 import { useState } from "react";
 import {
-  Container,
-  Button,
-  Box,
-  Typography,
-  FormControl,
-  InputLabel,
-  Input,
+    Container,
+    Button,
+    Box,
+    Typography,
+    FormControl,
+    InputLabel,
+    Input,
 } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { SHA512 } from "../utils/encryption";
 import users from "../api/users";
+import { loadUserFromToken } from "../utils/token";
+import { useDispatch } from "react-redux";
+import { changeUser } from "../types/actions";
 
 const LoginPage = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const navigator = useNavigate();
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const navigator = useNavigate();
+    const dispatch = useDispatch();
 
-  const handleLogin = () => {
-    const login = async () => {
-      if (await users.login(username, SHA512(password))) {
-        navigator("/forms");
-      }
+    const handleLogin = () => {
+        const login = async () => {
+            if (await users.login(username, SHA512(password))) {
+                const user = loadUserFromToken();
+                if (user) {
+                    dispatch(changeUser(user));
+                }
+                navigator("/forms");
+            }
+        };
+
+        login();
     };
 
-    login();
-  };
+    return (
+        <Container maxWidth="sm" style={{ marginTop: "50px" }}>
+            <Typography variant="h4" gutterBottom>
+                Login
+            </Typography>
+            <Box
+                component="form"
+                noValidate
+                autoComplete="off"
+                onSubmit={(event) => {
+                    event.preventDefault();
+                    handleLogin();
+                }}
+            >
+                <FormControl fullWidth margin="normal" required>
+                    <InputLabel htmlFor="username">Username</InputLabel>
+                    <Input
+                        id="username"
+                        type="text"
+                        value={username}
+                        onChange={(event) => setUsername(event.target.value)}
+                    />
+                </FormControl>
 
-  return (
-    <Container maxWidth="sm" style={{ marginTop: "50px" }}>
-      <Typography variant="h4" gutterBottom>
-        Login
-      </Typography>
-      <Box
-        component="form"
-        noValidate
-        autoComplete="off"
-        onSubmit={(event) => {
-          event.preventDefault();
-          handleLogin();
-        }}
-      >
-        <FormControl fullWidth margin="normal" required>
-          <InputLabel htmlFor="username">Username</InputLabel>
-          <Input
-            id="username"
-            type="text"
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-          />
-        </FormControl>
+                <FormControl fullWidth margin="normal" required>
+                    <InputLabel htmlFor="password">Password</InputLabel>
+                    <Input
+                        id="password"
+                        type="password"
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                    />
+                </FormControl>
 
-        <FormControl fullWidth margin="normal" required>
-          <InputLabel htmlFor="password">Password</InputLabel>
-          <Input
-            id="password"
-            type="password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </FormControl>
-
-        <Box mt={3}>
-          <Button
-            fullWidth
-            variant="contained"
-            color="primary"
-            size="large"
-            type="submit"
-          >
-            LOGIN
-          </Button>
-        </Box>
-      </Box>
-    </Container>
-  );
+                <Box mt={3}>
+                    <Button
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        size="large"
+                        type="submit"
+                    >
+                        LOGIN
+                    </Button>
+                </Box>
+            </Box>
+        </Container>
+    );
 };
 
 export default LoginPage;
--- src/views/RegisterPage.tsx
+++ src/views/RegisterPage.tsx
@@ -1,125 +1,139 @@
 import {
-  Container,
-  Button,
-  Box,
-  Typography,
-  FormControl,
-  InputLabel,
-  Input,
+    Container,
+    Button,
+    Box,
+    Typography,
+    FormControl,
+    InputLabel,
+    Input,
 } from "@mui/material";
 import users from "../api/users";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { SHA512 } from "../utils/encryption";
+import { useDispatch } from "react-redux";
+import { changeUser } from "../types/actions";
+import { loadUserFromToken } from "../utils/token";
 
 interface RegisterType {
-  label: string;
-  value: string;
-  setFunction: React.Dispatch<React.SetStateAction<string>>;
-  type: string;
+    label: string;
+    value: string;
+    setFunction: React.Dispatch<React.SetStateAction<string>>;
+    type: string;
 }
 
 const RegisterPage = () => {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const navigate = useNavigate();
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
 
-  const registerFields: RegisterType[] = [
-    {
-      label: "First name",
-      value: firstName,
-      setFunction: setFirstName,
-      type: "text",
-    },
-    {
-      label: "Last name",
-      value: lastName,
-      setFunction: setLastName,
-      type: "text",
-    },
-    { label: "Email", value: email, setFunction: setEmail, type: "email" },
-    {
-      label: "Username",
-      value: username,
-      setFunction: setUsername,
-      type: "text",
-    },
-    {
-      label: "Password",
-      value: password,
-      setFunction: setPassword,
-      type: "password",
-    },
-  ];
+    const registerFields: RegisterType[] = [
+        {
+            label: "First name",
+            value: firstName,
+            setFunction: setFirstName,
+            type: "text",
+        },
+        {
+            label: "Last name",
+            value: lastName,
+            setFunction: setLastName,
+            type: "text",
+        },
+        { label: "Email", value: email, setFunction: setEmail, type: "email" },
+        {
+            label: "Username",
+            value: username,
+            setFunction: setUsername,
+            type: "text",
+        },
+        {
+            label: "Password",
+            value: password,
+            setFunction: setPassword,
+            type: "password",
+        },
+    ];
 
-  const register = () => {
-    const handleRegister = async () => {
-      if (
-        await users.register(
-          firstName,
-          lastName,
-          username,
-          email,
-          SHA512(password)
-        )
-      ) {
-        navigate("/forms");
-      }
+    const register = () => {
+        const handleRegister = async () => {
+            if (
+                await users.register(
+                    firstName,
+                    lastName,
+                    username,
+                    email,
+                    SHA512(password)
+                )
+            ) {
+                const user = loadUserFromToken();
+                
+                if (user) {
+                    dispatch(changeUser(user));
+                }
+
+                navigate("/forms");
+            }
+        };
+
+        handleRegister();
     };
 
-    handleRegister();
-  };
+    return (
+        <Container maxWidth="sm" style={{ marginTop: "50px" }}>
+            <Typography variant="h4" gutterBottom>
+                Register
+            </Typography>
+            <Box
+                component="form"
+                noValidate
+                autoComplete="off"
+                onSubmit={(event) => {
+                    event.preventDefault();
+                    register();
+                }}
+            >
+                {registerFields.map((field, index) => (
+                    <FormControl
+                        key={index}
+                        fullWidth
+                        margin="normal"
+                        variant="outlined"
+                        required
+                    >
+                        <InputLabel htmlFor={field.label}>
+                            {field.label}
+                        </InputLabel>
+                        <Input
+                            id={field.label}
+                            type={field.type}
+                            value={field.value}
+                            required
+                            onChange={(event) =>
+                                field.setFunction(event.target.value)
+                            }
+                        />
+                    </FormControl>
+                ))}
 
-  return (
-    <Container maxWidth="sm" style={{ marginTop: "50px" }}>
-      <Typography variant="h4" gutterBottom>
-        Register
-      </Typography>
-      <Box
-        component="form"
-        noValidate
-        autoComplete="off"
-        onSubmit={(event) => {
-          event.preventDefault();
-          register();
-        }}
-      >
-        {registerFields.map((field, index) => (
-          <FormControl
-            key={index}
-            fullWidth
-            margin="normal"
-            variant="outlined"
-            required
-          >
-            <InputLabel htmlFor={field.label}>{field.label}</InputLabel>
-            <Input
-              id={field.label}
-              type={field.type}
-              value={field.value}
-              required
-              onChange={(event) => field.setFunction(event.target.value)}
-            />
-          </FormControl>
-        ))}
-
-        <Box mt={3}>
-          <Button
-            fullWidth
-            variant="contained"
-            color="primary"
-            size="large"
-            type="submit"
-          >
-            Sign Up
-          </Button>
-        </Box>
-      </Box>
-    </Container>
-  );
+                <Box mt={3}>
+                    <Button
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        size="large"
+                        type="submit"
+                    >
+                        Sign Up
+                    </Button>
+                </Box>
+            </Box>
+        </Container>
+    );
 };
 
 export default RegisterPage;
