2cfdc4b08bf9bfc708f0a2541761b09ec0fa15dc
yotam201410 <yotam201410@gmail.com>
2024-08-21 11:26:43 +0300

    home page buttons auto change if your logged in or registered

--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -1,7 +1,11 @@
 import { Container, Typography, Button, Box } from "@mui/material";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { AppState } from "../store/rootReducer";
 
 const HomePage = () => {
+  const user = useSelector((state: AppState) => state.user);
+
   return (
     <Container
       maxWidth="sm"
@@ -17,9 +21,9 @@ const HomePage = () => {
           size="large"
           style={{ marginRight: "20px", width: "150px" }}
           component={Link}
-          to="/login"
+          to={!user ? "/login" : "/forms"}
         >
-          Login
+          {!user ? <div>login</div> : <div>forms</div>}
         </Button>
         <Button
           variant="contained"
@@ -27,9 +31,9 @@ const HomePage = () => {
           size="large"
           style={{ width: "150px" }}
           component={Link}
-          to="/register"
+          to={!user?"/register":"forms/create"}
         >
-          Register
+          {!user ? <div>register</div> : <div>create form</div>}
         </Button>
       </Box>
     </Container>
