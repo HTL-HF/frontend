d2f397eceaf4a27780237b2d695e269b1fa4cbc2
yotam201410 <yotam201410@gmail.com>
2024-09-05 11:55:07 +0300

    added answer base

---
+++ src/components/answers/AnswerBase.tsx
@@ -0,0 +1,35 @@
+import { Box, styled, Typography } from "@mui/material";
+import React, { ReactNode } from "react";
+import { QuestionAnsweringModal } from "../../types/form";
+
+const AnswerBox = styled(Box)(({ theme }) => ({
+  border: `1px solid ${theme.palette.divider}`,
+  borderRadius: theme.shape.borderRadius,
+  padding: theme.spacing(2),
+  marginBottom: theme.spacing(2),
+  position: "relative",
+}));
+
+export interface AnswerBaseProps {
+  question: QuestionAnsweringModal;
+  children?: ReactNode;
+  answer: number | string;
+  disable: boolean;
+  onChange: (value: string | number | string[]) => void;
+}
+
+const AnswerBase: React.FC<AnswerBaseProps> = ({ question, children }) => {
+  return (
+    <AnswerBox>
+      <Typography variant="h5">{question.title}</Typography>
+      {question.description && (
+        <Typography variant="body1" marginTop="16px">
+          {question.description}
+        </Typography>
+      )}
+      {children}
+    </AnswerBox>
+  );
+};
+
+export default AnswerBase;
