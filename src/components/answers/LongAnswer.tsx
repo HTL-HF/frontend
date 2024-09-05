8019e7c46bd41435f4f7acb945cc6ecd8ba00aaa
yotam201410 <yotam201410@gmail.com>
2024-09-05 11:55:17 +0300

    added short and long answer

---
+++ src/components/answers/LongAnswer.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import AnswerBase, { AnswerBaseProps } from "./AnswerBase";
+import { FormControl, TextareaAutosize, TextField } from "@mui/material";
+
+interface LongAnswerProps extends AnswerBaseProps {}
+
+const LongAnswer: React.FC<LongAnswerProps> = ({
+  question,
+  answer,
+  disable,
+  onChange,
+}) => {
+  return (
+    <AnswerBase
+      answer={answer}
+      disable={disable}
+      onChange={onChange}
+      question={question}
+    >
+      <FormControl>
+        <TextField
+          disabled={disable}
+          value={answer}
+          multiline
+          InputProps={{
+            sx: {
+              "& .MuiOutlinedInput-root": {
+                "& textarea": {
+                  overflowY: "auto",
+                  resize: "none",
+                },
+              },
+            },
+          }}
+          onChange={(e) => {
+            onChange(
+              question.type === "number"
+                ? Number(e.target.value)
+                : e.target.value
+            );
+          }}
+          type={question.type}
+          minRows={5}
+        />
+      </FormControl>
+    </AnswerBase>
+  );
+};
+
+export default LongAnswer;
---
+++ src/components/answers/ShortAnswer.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import AnswerBase, { AnswerBaseProps } from "./AnswerBase";
+import { FormControl, TextField } from "@mui/material";
+
+interface AnswerShortProps extends AnswerBaseProps {}
+
+const ShortAnswer: React.FC<AnswerShortProps> = ({
+  question,
+  answer,
+  disable,
+  onChange,
+}) => {
+  return (
+    <AnswerBase
+      answer={answer}
+      disable={disable}
+      onChange={onChange}
+      question={question}
+    >
+      <FormControl>
+        <TextField
+          disabled={disable}
+          value={answer}
+          onChange={(e) => {
+            onChange(
+              question.type === "number"
+                ? Number(e.target.value)
+                : e.target.value
+            );
+          }}
+          type={question.type}
+        />
+      </FormControl>
+    </AnswerBase>
+  );
+};
+
+export default ShortAnswer;
