dbabede013b6fcfc83ebe475a47d3d8b323fb88c
yotam201410 <yotam201410@gmail.com>
2024-09-09 10:56:49 +0300

    added api calls

--- src/api/responses.ts
+++ src/api/responses.ts
@@ -25,3 +25,57 @@ export const sendResponse = async (
     }
   }
 };
+
+export const sendGetResponses = async (
+  formId: string,
+  navigator: (path: string) => void,
+  showNotification: (message: string, severity: AlertColor) => void
+) => {
+  try {
+    return (
+      await server.get(`/forms/${formId}/responses`, { withCredentials: true })
+    ).data;
+  } catch (err) {
+    if (err instanceof AxiosError) {
+      const statusMap = {
+        [StatusCodes.UNAUTHORIZED]: "You need to login!",
+        [StatusCodes.NOT_FOUND]: "No such form",
+      };
+
+      showError(err, statusMap, showNotification);
+
+      if (err.response?.status === StatusCodes.NOT_FOUND) {
+        navigator("/forms");
+      }
+    }
+  }
+};
+
+export const sendDeleteResponse = async (
+  formId: string,
+  responseId:string,
+  navigator: (path: string) => void,
+  showNotification: (message: string, severity: AlertColor) => void
+) => {
+  try {
+    await server.delete(`/forms/${formId}/responses/${responseId}`, {
+      withCredentials: true,
+    });
+    return true;
+  } catch (err) {
+    if (err instanceof AxiosError) {
+      const statusMap = {
+        [StatusCodes.UNAUTHORIZED]: "You need to login!",
+        [StatusCodes.FORBIDDEN]: "You are not owner of this form",
+        [StatusCodes.NOT_FOUND]: "No such form or response",
+      };
+
+      showError(err, statusMap, showNotification);
+
+      if (err.response?.status === StatusCodes.NOT_FOUND) {
+        navigator("/forms");
+      }
+    }
+  }
+  return false;
+};
